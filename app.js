d2ebbe850dfa09be7bd5cc70482e35e1de0eec84
Veeti Vänttinen <veeti.vanttinen@mercantus.fi>
2025-03-31 13:46:20 +0300

    Assignment 1 final

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   res.send('Welcome to my REST API!');
 });
 
+app.get('/api/v1/cat', (req, res) => {
+  res.json({
+    cat_id: 1,
+    name: "Kissa",
+    birthdate: "31.3.2020",
+    weight: "10kg",
+    owner: "Taikamies",
+    image: "https://loremflickr.com/320/240/cat"
+  });
+})
+
+app.use('/public', express.static('public'));
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
